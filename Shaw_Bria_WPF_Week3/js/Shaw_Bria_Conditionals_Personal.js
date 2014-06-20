7f0e306c019226f3a383950b886f90e647319996
bria.alyse <bria.alyse@fullsail.edu>
2014-06-19 23:29:06 -0400

    Comment
    If i don’t use argan oil, this comment shows what the console will
    print.

--- Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Personal.js
+++ Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Personal.js
@@ -10,5 +10,6 @@ if(hairArganOil){
     //if I use the Hair Argan Oil, console will print "Your hair looks nice today!"
     console.log("Your hair looks nice today!")
 }else{
+    //If I don't use Hair Argan Oil, console will print "Oh geese, your hair looks pretty damaged."
     console.log("Oh geese, your hair looks pretty damaged.")
 }
