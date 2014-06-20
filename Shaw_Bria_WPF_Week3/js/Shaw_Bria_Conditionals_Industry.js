4420b1d2fdeb0ecfba03271e2b0d3f3acf4da05a
bria.alyse <bria.alyse@fullsail.edu>
2014-06-19 22:51:49 -0400

    Created Html and JS Docs
    Created an html doc for my Industry, Personal and Wacky Conditionals.
    Also created a Conditional for each. I added the link for them to
    connect to each page. I also created headers for all of the Docs.

---
+++ Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Industry.js
@@ -0,0 +1,7 @@
+/**
+ * Bria Shaw
+ * WPF 1406 Section 01
+ * Conditional Industry
+ */
+
+var deadlineTimeMet = 15;
---
+++ Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Wacky.js
@@ -0,0 +1,5 @@
+/**
+ * Bria Shaw
+ * WPF 1406 Section 01
+ * Conditional Wacky
+ */
