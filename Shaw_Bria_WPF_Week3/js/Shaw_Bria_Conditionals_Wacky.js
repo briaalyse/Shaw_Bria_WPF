13c8f1bc0fe23b33e583f889e32203d085061837
bria.alyse <bria.alyse@fullsail.edu>
2014-06-19 23:39:30 -0400

    Committing Wacky Condtional
    Committing Wacky Conditional about how if someone were to wear heels
    less than 2 in tall, console would state you look short, or else it
    would say i look average height.
    in the ternary, if i am wearing a heel higher than average, it would
    state they looked really tall or else, it would state they looked
    average height.

--- Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Wacky.js
+++ Shaw_Bria_WPF_Week3/js/Shaw_Bria_Conditionals_Wacky.js
@@ -3,3 +3,17 @@
  * WPF 1406 Section 01
  * Conditional Wacky
  */
+
+
+var heelHeight = 3;
+var minHeelHeight = 5;
+var wedgeHeel = 1;
+
+if(heelHeight < minHeelHeight){
+    //if heel height is lower than min heel height, console will print "OMG, you look so short!"
+    console.log("OMG, you look so short!")
+}else{
+    console.log("You look about average height.")
+}
+
+(heelHeight > minHeelHeight && wedgeHeel) ? console.log("You look so tall today!") : console.log("You look about average height.")
